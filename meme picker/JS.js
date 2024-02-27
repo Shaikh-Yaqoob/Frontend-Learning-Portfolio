fe73c06117e813e78cb89c3a9ea1b44b8bd0c7f1
AliKhanMagsi <alikhan.bscsf21kdk@iba-suk.edu.pk>
2024-02-28 00:30:57 +0500

    added a project that generate random meme on emotion

---
+++ meme picker/JS.js
@@ -0,0 +1,24 @@
+
+    let happy = document.getElementById("happy")
+    let memeBox = document.getElementById("meme")
+
+    happy.addEventListener("click", function(){
+
+        memeBox.innerHTML = `
+        <main>
+        <section id="meme">
+          <span>X</span>
+          <img src="images/happy.gif" alt="" id="meme-img">
+        </section>
+      </main>
+        
+        `
+    
+    
+    })
+
+
+ 
+
+
+
